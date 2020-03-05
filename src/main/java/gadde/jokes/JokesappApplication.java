4747c5d3a5b96997470347a662eee20cad603ffc
sgadde <gaddesurendrakumar@gmail.com>
2020-03-05 14:41:57 +0530

    Initial commit

---
+++ src/main/java/gadde/jokes/JokesappApplication.java
@@ -0,0 +1,13 @@
+package gadde.jokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokesappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JokesappApplication.class, args);
+    }
+
+}
